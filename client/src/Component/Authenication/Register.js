905776d7de63f104d79a3099de0c5e1cb2e66437
Kunyan Feng <fky198615@126.com>
2021-03-26 22:51:35 -0700

    update signIn register NavBar

---
+++ client/src/Component/Authenication/Register.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './Register.css';
+
+const Register = (props) => {
+    return (
+        <div>
+            <h1>Register</h1>
+            <Form className="RegisterContainer">
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Confirm password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    )
+}
+
+export default Register;
