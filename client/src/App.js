267343b53e5db7ac8aaa25e21f5fd21df5be3122
RomSham <80100691+RomSham@users.noreply.github.com>
2021-04-08 12:02:44 +0200

    add folders pages (for more than one component), actions, reducer

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,20 @@ import React from "react";
 import './App.css';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Register from './Component/Authenication/Register';
-// import SignIn from './Component/Authenication/Signin';
-import NavBar from './Component/Navbar/Navbar';
+
 import Cart from './Component/cart';
 
 import Header from "./Component/header";
-import HomePage from "./Component/homePage";
+import HomePage from "./pages/homePage";
 import Register from "./Component/register";
 import SignIn from "./Component/signIn";
-import CategoryA from "./Component/categoryA";
-import CategoryA1 from "./Component/categoryA1";
-import CategoryA2 from "./Component/categoryA2";
-import CategoryA3 from "./Component/categoryA3";
-import Product from "./Component/product";
+import CategoryA from "./pages/categoryA";
+import CategoryA1 from "./pages/categoryA1";
+import CategoryA2 from "./pages/categoryA2";
+import CategoryA3 from "./pages/categoryA3";
+
 import Footer from "./Component/footer";
+import ProductPage from "./pages/productPage";
 
 
 function App() {
@@ -34,7 +33,7 @@ function App() {
           <Route path='/categoryA1' exact component={CategoryA1}/>
           <Route path='/categoryA2' exact component={CategoryA2}/>
           <Route path='/categoryA3' exact component={CategoryA3}/>
-          <Route path='/product' exact component={Product}/>
+          <Route path='/productPage' exact component={ProductPage}/>
           <Route path='/cart' exact component={Cart}/>
 
 
