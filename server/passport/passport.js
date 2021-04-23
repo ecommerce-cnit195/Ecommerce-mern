dc3b8b3518854a89b7770b24d2f776489cb1c1c8
hatout1 <hatout1@gmail.com>
2021-04-23 06:02:58 -0700

    adding a script to run server from global, preparing for Heroku deployment

--- server/passport/passport.js
+++ server/passport/passport.js
@@ -27,3 +27,31 @@ passport.use(
     }
   )
 );
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username }, (err, user) => {
+      // something went wrong with database
+      if (err) return done(err);
+      // if no user exist
+      if (!user) return done(null, false);
+      // check if password is correct
+      user.comparePassword(password, done);
+    });
+  })
+);
+
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function (err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// ));
