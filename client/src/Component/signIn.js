1e31ec4907dfc2ab0757497b199cec47156de59b
hatout1 <hatout1@gmail.com>
2021-04-25 02:50:04 -0700

    updated regiter and sigin pages, finished the AuthContext and Messages, still need to work on custom errors for the login username-password errors

--- client/src/Component/signIn.js
+++ client/src/Component/signIn.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React , {useContext , useState} from 'react';
 import {Form, Button, Container} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
-
+import {AuthContext} from "../Context/AuthContext"
+import AuthService from "../Service/AuthService"
+import Message from "./Messages"
 
 
 const SignIn = (props) => {
+
+    const [user,setUser] = useState({username:"", password:""})
+    const [message, setMessage] = useState (null)
+    const authContext = useContext(AuthContext)
+
+    const onChange = (e) =>{
+        setUser({ ...user, [e.target.name]: e.target.value })
+    } 
+    const onSubmit =(e)=>{
+        e.preventDefault()
+        AuthService.login(user).then((data)=>{
+            const {isAuhtentecated, user,message} = data
+            if(isAuhtentecated){
+                authContext.setUser(user)
+                authContext.isAuhtentecated(isAuhtentecated)
+                props.history.push("/")
+            } else setMessage(message)
+        })
+    }
+
+
     return (
         <Container  className="justify-content-center p-5" >
             <h1 className='pt-5'>Sign In</h1>
-            <Form className="p-3">
+            {message ? <Message message={message} /> : null}
+            <Form className="p-3" onSubmit={onSubmit}>
                 <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control 
+                    className="input"
+                    type="text" 
+                    name="username"
+                    onChange={onChange}
+                    placeholder="Enter username" 
+                    />
+                    {/* <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
-                    </Form.Text>
+                    </Form.Text> */}
                 </Form.Group>
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control 
+                    classNmae="input"
+                    name="password"
+                    type="password" 
+                    placeholder="Password" 
+                    onChange={onChange}
+                    />
                 </Form.Group>
                 <Button variant="warning" type="submit">
                     Submit
@@ -28,6 +64,8 @@ const SignIn = (props) => {
                 <p>New Customer?</p>
                 <Link to="/register">Register</Link>
             </div>
+
+
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </Container>
     )
